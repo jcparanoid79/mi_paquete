d409871229477b80d3b396253afc9176e911a504
Juan Garcia <jcparanoid79@hotmail.com>
2025-04-14 16:08:18 -0600

    correction in Pytest module

--- test/test_modulo1.py
+++ test/test_modulo1.py
@@ -1,5 +1,7 @@
-import pytest
-from mi_paquete.modulo1 import saludar
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from src.modulo1 import saludar
 
 def test_saludar_nombre_normal():
     """Test saludar with a regular name"""
