aeb80c9d1e43e5160bc67331cd4553ee4d976bfd
Juan Garcia <jcparanoid79@hotmail.com>
2025-04-14 14:48:42 -0600

    New features

--- src/modulo1.py
+++ src/modulo1.py
@@ -2,3 +2,8 @@
 def saludar(nombre):
   """Saluda a alguien."""
   return f"¡Hola, {nombre}!"
+
+if __name__ == "__main__":
+  # Código de prueba para ejecutar directamente
+  nombre = input("Introduce tu nombre: ")
+  print(saludar(nombre))
